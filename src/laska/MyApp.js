c14b82880a176a832bf3c8191a0af805d3a14f8e
Hamza Ali <hamxa1331@gmail.com>
2018-12-16 04:04:37 +0500

    Basic App
    Basic App codebase completed, UI tweaks remaining.

--- src/laska/MyApp.js
+++ src/laska/MyApp.js
@@ -8,7 +8,8 @@ import {
   ScrollView,
   StatusBar,
   Alert,
-  CameraRoll
+  CameraRoll,
+  TouchableWithoutFeedback
 } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {styles} from "./styles";
@@ -20,9 +21,25 @@ class MyApp extends React.Component {
   static navigationOptions = { title: "My App" };
   constructor(props) {
     super(props);
-    this.state = {};
-    this.state = { chosenDate: new Date(),image:'',images:[],galleryImages:[],showSubmit:true };
+    this.initial ={
+      chosenDate: new Date(),
+      image:'',
+      images:[],
+      galleryImages:[],
+      showSubmit:true,
+      name:'',
+      email:'',
+      location:'',
+      description:'',
+      ccInputs:[],
+      ccEmails:'',
+      showCC:false
+    }
+    this.state = this.initial
     this.setDate = this.setDate.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this)
+    this.renderAddButton=this.renderAddButton.bind(this)
+    this.handleMultipleEmails=this.handleMultipleEmails.bind(this)
   }
   setDate(newDate) {
     this.setState({ chosenDate: newDate });
@@ -81,6 +98,79 @@ class MyApp extends React.Component {
       });
     }
   };
+  async handleSubmit(){
+    if(this.state.description&&this.state.chosenDate&&this.state.email&&this.state.name&&this.state.galleryImages.length>0){
+      let images = this.state.galleryImages
+      let apiUrl = 'http://192.168.43.25:8080/users';
+      let formData = new FormData();
+      formData.append('name',this.state.name)
+      formData.append('email',this.state.email)
+      formData.append('description',this.state.description)
+      formData.append('location',this.state.location)
+      formData.append('date',this.state.chosenDate.toString())
+         if(this.state.ccEmails.length>0){
+          let inputs = JSON.stringify(this.state.ccEmails.split('\n'))
+          console.log(inputs)
+          formData.append('cc',inputs)    
+         }            
+      images.forEach((image,index)=>{
+      let uri = image.imageUrl
+      let uriParts = uri.split('.');
+      let fileType = uriParts[uriParts.length - 1];
+    
+      formData.append('photo', {
+        uri,
+        name: `photo#${index}.${fileType}`,
+        type: `image/${fileType}`,
+      });    
+    })
+        let options = {
+          method: 'POST',
+          body: formData,
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'multipart/form-data',
+          },
+        };
+          fetch(apiUrl, options).then(res=>res.json()).then(data=>{
+            Alert.alert('Success',data.message)
+            this.setState({
+              chosenDate: new Date(),
+              image:'',
+              images:[],
+              galleryImages:[],
+              showSubmit:true,
+              name:'',
+              email:'',
+              location:'',
+              description:'',
+              ccInputs:[],
+              ccEmails:'',
+              showCC:false
+            })
+         }).catch(err=>{
+           Alert.alert('Failed',err)
+         });
+    }
+    else
+    Alert.alert('Falied','Fill all the values..')
+  }
+  renderAddButton(){
+    return(
+      <TouchableWithoutFeedback onPress={this.handleMultipleEmails}>
+      <Icon
+          name='plus'
+          size={20}
+          style={{marginLeft:5,marginTop:15}}
+        />
+      </TouchableWithoutFeedback>
+    )
+  }
+  handleMultipleEmails(){
+   this.setState({
+     showCC:true
+   })
+  }
   render() {
     return (
       <Fragment>
@@ -91,13 +181,25 @@ class MyApp extends React.Component {
           </View>
           <ScrollView style={styles.sc1a4da9b}>
             <View>
-              <TextInput autoFocus={true}  underlineColorAndroid='transparent' keyboardType='default' placeholder={`Client Name`} style={styles.s7ff692e2} />
+              <TextInput value={this.state.name} autoFocus={true} onChangeText={text=>this.setState({name:text})} underlineColorAndroid='transparent' keyboardType='default' placeholder={`Client Name`} style={styles.s7ff692e2} />
             </View>
-            <View>
-              <TextInput underlineColorAndroid='transparent' placeholder={`Email`} style={styles.sa5239b01} keyboardType='email-address' />
+           <View style={{flexDirection:'row',paddingTop:10}}>
+              <TextInput value={this.state.email} onChangeText={text=>this.setState({email:text})} underlineColorAndroid='transparent' placeholder={`Email`} style={styles.sa5239b01} keyboardType='email-address' />
+            {!this.state.showCC&&this.renderAddButton()}
             </View>
+            {this.state.showCC&&<View>
+              <TextInput 
+              underlineColorAndroid='transparent'
+                multiline={true}
+                numberOfLines={4}
+                placeholder={`CC Emails`}
+                style={styles.s6dd4cd00}
+                onChangeText={text=>this.setState({ccEmails:text})}
+                value={this.state.ccEmails}
+              />
+            </View>}         
             <View>
-              <TextInput  underlineColorAndroid='transparent'placeholder={`Location`} style={styles.s96e03553} />
+              <TextInput value={this.state.location} onChangeText={text=>this.setState({location:text})} underlineColorAndroid='transparent'placeholder={`Location`} style={styles.s96e03553} />
             </View>
             <View>
               <TextInput 
@@ -106,6 +208,8 @@ class MyApp extends React.Component {
                 numberOfLines={4}
                 placeholder={`Project Description`}
                 style={styles.s6dd4cd00}
+                onChangeText={text=>this.setState({description:text})}
+                value={this.state.description}
               />
             </View>
             <View style={styles.sabb47c64}>
@@ -135,7 +239,7 @@ class MyApp extends React.Component {
         </ScrollView>}
             {this.state.images.length>0 && <ImageGallery/>}
             {this.state.showSubmit&&<View style={styles.sc9f26d46}>
-              <Button title={`Submit`} onPress={() => {}} />
+              <Button title={`Submit`} onPress={this.handleSubmit} />
             </View>}
           </ScrollView>
         </View>
@@ -143,10 +247,5 @@ class MyApp extends React.Component {
     );
   }
 }
-MyApp.defaultProps = {};
-
 MyApp = withNavigationProp(MyApp);
-
 export default MyApp;
-
-export { styles };
